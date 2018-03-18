ecf880c6a96d15789da384f281eb152cd43fe8d4
Unknown <nicolasrich@hotmail.fr>
2018-03-18 18:43:40 +0100

    Enable scheduling

--- ott/src/main/OttApplication.java
+++ ott/src/main/OttApplication.java
@@ -2,8 +2,10 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OttApplication {
 
 	public static void main(String[] args) {
