f00880faaee9c1bb34a35c02724cc967e225c910
Ismaïl Ouazzany <ouazzany.i@gmail.com>
2018-03-18 17:28:28 +0100

    Hibernate config - fichier manquant

---
+++ ott/src/main/HibernateConfig.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ImportResource({"applicationContext.xml"})
+public class HibernateConfig {
+}
