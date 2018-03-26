55eb240776b36f21c290c40b1362060c7ad3032e
Guirriec <guirriec.guillaume@gmail.com>
2018-03-26 15:24:10 +0200

    Add Kafka Configuration.

---
+++ src/main/java/org/emn/fila1/spring/cop/service/KafkaConfiguration.java
@@ -0,0 +1,37 @@
+package org.emn.fila1.spring.cop.service;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        Map<String, Object> senderProps = senderProps();
+        ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<String, String>(senderProps);
+        KafkaTemplate<String, String> template = new KafkaTemplate<>(pf);
+        return template;
+    }
+
+}
