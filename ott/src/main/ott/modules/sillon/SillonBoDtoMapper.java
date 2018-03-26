5e4df577782f5ed138a4ca53c8f33806ea95dbc3
Unknown <nicolasrich@hotmail.fr>
2018-03-26 16:09:51 +0200

    Sillon mapper

--- ott/src/main/ott/modules/sillon/SillonBoDtoMapper.java
+++ ott/src/main/ott/modules/sillon/SillonBoDtoMapper.java
@@ -1,13 +1,26 @@
 package main.ott.modules.sillon;
 
 import main.ott.modules.base.Mapper;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SillonBoDtoMapper extends Mapper<SillonBo, SillonDto> {
+	
+	private ModelMapper mapper;
 
 	public SillonBoDtoMapper() {
-		super(SillonBo.class, SillonDto.class);
+		mapper = new ModelMapper();
+	}
+	@Override
+	public SillonDto bo2Dto(SillonBo bo) {
+		return mapper.map(bo, SillonDto.class);
+	}
+
+	@Override
+	public SillonBo dto2Bo(SillonDto dto) {
+		return mapper.map(dto, SillonBo.class);
 	}
 
 }
